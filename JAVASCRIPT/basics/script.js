a9a853c591ae0c1e5f0c020d42dd7273360e9779
prerika25 <prerikagarg89@gmail.com>
2026-07-05 18:27:03 +0530

    practice

---
+++ JAVASCRIPT/basics/script.js
@@ -0,0 +1,15 @@
+alert("Hello World");
+console.log("code is running...");
+console.log("code is also running...");
+console.log("code is wow running...");
+
+var a=prompt("Enter your number");
+var isTrue=confirm("Are you sure you want to submit?");
+
+if(isTrue){
+    console.log("You have submitted your name");
+}
+else{
+    console.log("You have not submitted your name");
+}
+console.log("Your number is " + a);
