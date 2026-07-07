aa104f13a3ccbc5d32e2ff8c124666dd72247bf7
prerika25 <prerikagarg89@gmail.com>
2026-07-07 18:05:39 +0530

    practice

---
+++ JAVASCRIPT/Dom/script.js
@@ -0,0 +1,9 @@
+console.log("Hello Wrold");
+
+document.body.firstElementChild;
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
+document.body.firstElementChild.children[3].nextElementSibling
+document.body.firstElementChild.children[3].previousElementSibling
+document.body.firstElementChild.children[3].previousSibling
+document.body.firstElementChild.children[3].parentElement
---
+++ JAVASCRIPT/byids/script.js
@@ -0,0 +1,10 @@
+console.log("Prerika");
+let boxes=document.getElementsByClassName("box")
+console.log(boxes);
+//boxes[2].style.backgroundColor="red";
+//document.getElementById("red").style.backgroundColor="red";
+document.querySelector(".box").style.backgroundColor="green" 
+console.log(document.querySelectorAll(".box"))
+document.querySelectorAll(".box").forEach(e=>{
+    e.style.backgroundColor="green";
+})                   
