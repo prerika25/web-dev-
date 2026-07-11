937194503672aaf8a9fb5930596db6541618cf6e
prerika25 <prerikagarg89@gmail.com>
2026-07-11 12:27:01 +0530

    practice

---
+++ JAVASCRIPT/advancejs/script.js
@@ -0,0 +1,35 @@
+console.log("Advance JS")
+async function sleep(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(45);
+        },1000);
+    })
+}
+
+function sum(a,b,c){
+    return a+b+c 
+}
+(async function main(){
+    console.log(a1)
+    // let a=await sleep()
+    // console.log(a)
+    // let b=await sleep()
+    // console.log(b);
+
+    // let [x,y,...rest]=[1,5,7,8,9] //destructing
+    // console.log(x,y,rest)
+    let obj={
+        a:1,
+        b:2,
+        c:3
+    }
+    var a1=6 //hosting
+    let {a,b}=obj
+    console.log(a,b)
+    let arr=[1,4,6]
+    console.log(arr[0]+arr[1]+arr[2])
+    console.log(sum(...arr))  //spread operator    
+})()
+
+
