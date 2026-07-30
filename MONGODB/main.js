b501907b646abccba1b98b21efd85fbb8ec8f145
prerika25 <prerikagarg89@gmail.com>
2026-07-30 18:54:40 +0530

    practice

---
+++ MONGODB/main.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import express from "express";
+import {Todo} from "./models/Todo.js";             
+let conn=await mongoose.connect("mongodb://localhost:27017/")
+
+const app=express();
+
+const port=3000;
+app.get('/',(req,res)=>{
+    const todo=new Todo({title:"Hey first todo",desc="Description of this todo",isDone: false })
+    todo.save()
+    res.send('Hello World!')
+})
+app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
