2aabf1cae74db951a86f05387c88ccb82e28c0b6
prerika25 <prerikagarg89@gmail.com>
2026-07-30 17:22:01 +0530

    practice

---
+++ MONGODB/crud.mongodb.js
@@ -0,0 +1,88 @@
+use("CrudDb")
+
+// db.createCollection("courses")
+// db.courses.insertOne({
+//     name:"Prerika web dev",
+//     price: 0,
+//     assignments:12,
+//     projects:45
+// })
+
+//db.courses.insertMany([
+//   {
+//     name: "Java Masterclass",
+//     price: 499,
+//     assignments: 20,
+//     projects: 8
+//   },
+//   {
+//     name: "Python Bootcamp",
+//     price: 799,
+//     assignments: 18,
+//     projects: 10
+//   },
+//   {
+//     name: "C++ DSA",
+//     price: 999,
+//     assignments: 30,
+//     projects: 15
+//   },
+//   {
+//     name: "React.js Complete",
+//     price: 1499,
+//     assignments: 25,
+//     projects: 12
+//   },
+//   {
+//     name: "Node.js & Express",
+//     price: 1299,
+//     assignments: 22,
+//     projects: 9
+//   },
+//   {
+//     name: "MongoDB Essentials",
+//     price: 699,
+//     assignments: 10,
+//     projects: 5
+//   },
+//   {
+//     name: "Full Stack MERN",
+//     price: 2999,
+//     assignments: 40,
+//     projects: 20
+//   },
+//   {
+//     name: "AI for Beginners",
+//     price: 1999,
+//     assignments: 16,
+//     projects: 7
+//   },
+//   {
+//     name: "Machine Learning Basics",
+//     price: 2499,
+//     assignments: 28,
+//     projects: 14
+//   }
+// ]);
+// READ
+// let a =db.courses.find({price:0})
+// console.log(a)
+//console.log(a.count())
+//console.log(a.toArray())
+
+//UPDATE
+// db.courses.updateOne({price:0},{$set:{price:100}})
+//db.courses.updateMany({price:0},{$set:{price:100}})
+
+
+//DELETE
+// db.courses.deleteOne({price:100})
+// db.courses.deleteMany({price:2999})
+
+
+
+
+
+
+
+
