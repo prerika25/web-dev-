477e0c539c31f565dba0bf2351a40ba94c71dd9a
prerika25 <prerikagarg89@gmail.com>
2026-08-14 16:21:26 +0530

    practice

--- REACT/with-react/src/App.js
+++ REACT/with-react/src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css"
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 
 function App() {
+  const [value, setValue] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar logoText="PRERIKACODE "/>
+      <div className='value'> {value}</div>
+     <button onClick={()=>{setValue(value + 1)}}>Click me</button>
+     <Footer/>
     </div>
   );
 }
 
-export default App;
+export default App;            
