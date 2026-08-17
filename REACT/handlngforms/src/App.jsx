41ea4987b2b590166cc5e62452f0ac3a866fc54c
prerika25 <prerikagarg89@gmail.com>
2026-08-17 20:54:35 +0530

    react

---
+++ REACT/handlngforms/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <form action="">
+          <input type="text" name="username" id="" />
+          <input type="password" name="password" id="" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default App
